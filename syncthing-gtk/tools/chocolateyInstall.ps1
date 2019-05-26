a9b11fa1cb4bde860badcce904e07a15eddd5bfe
CoolenXue <646099228@qq.com>
2019-05-26 16:43:04 +0800

    Clean syncthing from study code.

--- syncthing-gtk/tools/chocolateyInstall.ps1
+++
@@ -1,7 +0,0 @@
-$packageName = 'syncthing-gtk'
-$installerType = 'EXE'
-$url = ''
-$silentArgs = '/S'
-$validExitCodes = @(0)
-
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
--- syncthing/man/README.md
+++
@@ -1,3 +0,0 @@
-These manual pages are generates on https://docs.syncthing.net/, based on
-the https://github.com/syncthing/docs repo. Do not edit them in this
-repo.
